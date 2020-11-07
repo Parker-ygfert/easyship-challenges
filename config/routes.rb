83be47c7487de285ba8281ff2539229423e8b871
Parker <ygfert@gmail.com>
2020-11-07 21:48:44 +0800

    feat: add ShipmentController show action api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :companies, only: [] do
+    resources :shipments, only: %i[index show]
+  end
 end
