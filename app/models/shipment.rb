a2cf46f9a3dba8436916a96c5d42a7f7c92a01bf
Parker <ygfert@gmail.com>
2020-11-07 22:37:36 +0800

    feat: add group_shipment_items method in Shipment model

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,4 +1,10 @@
 class Shipment < ApplicationRecord
   belongs_to :company
   has_many :shipment_items
+
+  def group_shipment_items
+    shipment_items.group(:description).order('count_all desc').count.map do |item|
+      {description: item[0], count: item[1]}
+    end
+  end
 end
