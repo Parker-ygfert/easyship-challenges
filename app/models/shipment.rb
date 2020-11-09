13235198377b451d9905123caecbd4ad09808e47
Parker <ygfert@gmail.com>
2020-11-09 16:18:47 +0800

    fix: add company_name delegate method to Shipment model

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,6 +2,8 @@ class Shipment < ApplicationRecord
   belongs_to :company
   has_many :shipment_items
 
+  delegate :name, to: :company, prefix: true
+
   def group_shipment_items
     shipment_items.group(:description).order('count_all desc').count.map do |item|
       {description: item[0], count: item[1]}
